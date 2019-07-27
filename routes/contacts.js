c73831f4d0c1ec3c4ed61436405d9f234a235faa
Prashant Bhat <prashantbhat84@gmail.com>
2019-07-27 13:10:30 +0530

    add contact to user account and get all contacts for logged in user - backend done

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,17 +1,51 @@
 const express=require('express');
 const router= express.Router();
+const User = require('../models/User');
+const Contact = require('../models/Contacts');
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 
 //@route     GET api/contacts
 //@desc     Get all contacts of a purticular user
 //@acess     private
-router.get('/',(req,res)=>{
-    res.send('Get user contacts');
+router.get('/', auth, async (req,res)=>{
+    try {
+        const contacts =await Contact.find({user:req.user.id}).sort({date:-1});
+        res.json(contacts);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+        
+    }
 });
 //@route     POST api/contacts
 //@desc     Insert a contact for a purticular user
 //@acess     private
-router.post('/',(req,res)=>{
-    res.send('inserted new contact  for logged in user');
+router.post('/', [auth,[
+    check('name','Name is required').not().isEmpty(),
+    check('email','Include a valid email').isEmail()
+
+]],async (req,res)=>{
+    const errors=validationResult(req);
+   if(!errors.isEmpty()){
+       res.status(400).json({
+           errors:errors.array()
+       })
+   }
+   const {name,email,phone,type}= req.body;
+   try {
+       const  newContact = new Contact({
+           name,email,phone,type,user:req.user.id
+       });
+     const contact = await newContact.save();
+     res.json(contact);
+       
+   } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+   }
+    
 });
 //@route     PUT api/users/:id
 //@desc     updates a purticular contact belonging to the user logged in
