2bb5572532b981343966aa73fc046962329db9a7
Prashant Bhat <prashantbhat84@gmail.com>
2019-07-30 13:55:56 +0530

    navbar done

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React,{Fragment}from 'react';
+import NavBar from './components/Layout/Navbar/Navbar';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 import './App.css';
 
 const  App=()=> {
   return (
-    <div className="App">
-      My App
-    </div>
+    <Router>
+      <Fragment>
+        <NavBar/>
+      <Switch>
+              <Route exact path='/' component={Home} />
+              
+              <Route path='/about' component={About} />
+          </Switch>
+
+          </Fragment>
+    
+  
+    </Router>
   );
 }
 
