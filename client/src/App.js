f99ebb3baa5640deac60cc340d56404c936245f0
Prashant Bhat <prashantbhat84@gmail.com>
2019-08-02 15:35:12 +0530

    front end login and register pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,34 @@
-import React,{Fragment} from 'react';
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom';
-import Home  from './pages/Home';
-import About from './pages/About';
-import ContactState from './context/contact/ContactState';
-import AuthState from './context/auth/AuthState';
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
 
-
-
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <AuthState>
-  <ContactState>
-    <Router>
-    <Fragment>
-      <Navbar/>
-      <div class="container">
-   <Switch>
-     <Route exact path="/" component={Home}/>
-     <Route exact path="/about" component={About}/>
-   </Switch>
- </div>
-    </Fragment>
-    </Router>
-    </ContactState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route excat path="/register" component={Register} />
+                <Route excat path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
     </AuthState>
-   
   );
 }
 
