d33eceb5f19abac648de5c77eccbffaf259520e8
Prashant Bhat <prashantbhat84@gmail.com>
2019-08-02 12:39:40 +0530

    fade effect css transition

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React,{Fragment,useContext} from 'react';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import ContactItem from './ContactItem';
 
 import ContactContext from '../../context/contact/ContactContext';
@@ -13,10 +14,11 @@ const Contacts = () => {
 
     return (
         <Fragment>
+            <TransitionGroup>
            {filtered!==null
-             ? filtered.map(contact=>(<ContactItem key={contact.id} contact={contact}/>)):
-             contacts.map(contact=>(<ContactItem key={contact.id} contact={contact}/>))}
-           
+             ? filtered.map(contact=>(<CSSTransition key={contact.id} timeout={500} classNames="item"><ContactItem  contact={contact}/></CSSTransition>)):
+             contacts.map(contact=>(<CSSTransition key={contact.id} timeout={500} classNames="item"><ContactItem  contact={contact}/></CSSTransition>))}
+           </TransitionGroup>
         </Fragment>
     )
 }
