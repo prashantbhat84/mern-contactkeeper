8e2e8f993f917ad219ad7e7eefdc2704758da5ab
Prashant Bhat <prashantbhat84@gmail.com>
2019-08-08 11:57:47 +0530

    added prod configs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 
 let app = express();
 connectDB();
@@ -18,13 +19,21 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
   res.json({ msg: 'welcome to contact keeper API' });
-});
+}); */
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
